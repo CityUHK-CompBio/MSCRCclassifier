3ee3cacf405243a45ca7cfd38df58aca24ca7bff
Carpentierbio <83494355+Carpentierbio@users.noreply.github.com>
2024-08-29 10:21:47 +0800

    Add files via upload

---
+++ R/example_exp.R
@@ -0,0 +1,7 @@
+#' Example Expression Data
+#'
+#' A dataset containing example expression data for testing.
+#'
+#' @format A data frame with z-score scaled values, samples in rows and multi-omics features in columns.
+#' @source the scaled expression of mRNA data.
+"example_exp"
