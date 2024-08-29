21b8323e2a41c6b80639781c1ede95b8082a3323
Carpentierbio <83494355+Carpentierbio@users.noreply.github.com>
2024-08-29 10:17:26 +0800

    Delete R directory

--- R/example_exp.R
+++
@@ -1,7 +0,0 @@
-#' Example Expression Data
-#'
-#' A dataset containing example expression data for testing.
-#'
-#' @format A data frame with samples in rows and multi-omics features in columns.
-#' @source the scaled expression of mRNA data (single omics data).
-"example_exp"
